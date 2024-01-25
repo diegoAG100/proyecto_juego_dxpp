4c88ff8987809adea76b0f6cbadc3993558a7087
diegoAG100 <diegoamenedogonzalez01@gmail.com>
2024-01-25 21:47:18 +0100

    solucionando error que la segunda vez que busca la posicion de los vasos no la encuntra por que se vacia el array

--- desenvolvimiento/proyecto_minimo/Assets/Scripts/Vasos/VasosPoiscion.cs
+++ desenvolvimiento/proyecto_minimo/Assets/Scripts/Vasos/VasosPoiscion.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class VasosPoiscion : MonoBehaviour
 {
+    static public VasosPoiscion instance;
+    public List<Transform> positions;
+    public List<Transform> adjudiquePositions;
+
+    void Awake(){
+        Debug.Log("1");
+        instance = this;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +23,22 @@ public class VasosPoiscion : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(adjudiquePositions.Count >3){
+            positions.Clear();
+            foreach(Transform t in adjudiquePositions){
+                positions.Add(t);
+            }
+            adjudiquePositions.Clear();
+        }
+
         
+    }
+
+    public Transform GivePosition(){
+        Debug.Log(positions.Count);
+        Transform poitionSend= positions[Random.Range(0,positions.Count)];
+        adjudiquePositions.Add(poitionSend);
+        positions.Remove(poitionSend);
+        return poitionSend;
     }
 }
---
+++ desenvolvimiento/proyecto_minimo/Assets/Vaso.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Vaso : MonoBehaviour
+{
+    public Transform nextPosition;
+    float timer = 2;
+
+    bool move = false;
+    void OnEnable(){
+        Invoke("NewPosition",0.1f);
+    }
+
+    public void NewPosition(){
+        nextPosition = VasosPoiscion.instance.GivePosition();
+        move=true;
+        timer = 3;
+    }
+
+    void Update(){
+        if(move){
+            transform.position = new Vector2(Mathf.Lerp(transform.position.x,nextPosition.position.x,0.01f),Mathf.Lerp(transform.position.y,nextPosition.position.y,0.05f));
+        }
+        if(Vector2.Distance(transform.position,nextPosition.position)<0.1f&&Vector2.Distance(transform.position,nextPosition.position)>-0.1f&&timer<0){
+            move =false;
+            NewPosition();
+        }
+        timer-=Time.deltaTime;
+    }
+}
